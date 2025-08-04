51de7cd521d0ce1f88a9abcd6219ed68a60bf631
Gustian3051 <gustianprayoga31@gmail.com>
2025-08-05 02:15:15 +0700

    ..

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,21 @@
 package main
 
-import "tes-app/internal/config"
+import (
+	"fmt"
+	"tes-app/database"
+	"tes-app/internal/config"
+	"tes-app/internal/routes"
+)
 
 func main() {
-	// Load config
 	config.LoadConfig()
+	database.ConnectPostgres()
 
+	r := routes.InitRoutes()
+	
+	port := ":8080"
+	
+	fmt.Printf("Server running at: http://localhost:%s\n", port)
+	
+	r.Run(port)
 }
